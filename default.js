1cafb3c887762071c93eaea92f9aa0712ee6327d
Gaurav <95282351+Gaurav-80@users.noreply.github.com>
2023-04-10 04:46:44 -0600

    update

--- default.js
+++ default.js
@@ -3,7 +3,7 @@ import { products } from "./constants/product.js";
 
 const DefaultData = async () => {
   try {
-    await Product.deleteMany({});
+//     await Product.deleteMany({});
     await Product.insertMany(products);
 
     console.log("Data imported Successfully");
